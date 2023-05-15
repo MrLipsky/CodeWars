653784e9e306ff61ed41420ee2b49b628861441f
MrLipsky <michallipski94@gmail.com>
2023-05-15 16:14:22 +0200

    Updated method with a use of string format() method

--- src/dev/cw/RgbToHex.java
+++ src/dev/cw/RgbToHex.java
@@ -4,26 +4,16 @@ public class RgbToHex {
 
     public static void main(String[] args) {
 
-        System.out.println(rgb(255,254,16));
+        System.out.println(rgb(255,254,16)); // equals to FFFE10 in hexadecimal
+        System.out.println(rgb(255,255,255)); // equals to FFFFFF in hexadecimal
+        System.out.println(rgb(0,0,0)); // equals to 000000 in hexadecimal
 
     }
 
 //    Method converting RGB color values to hexadecimal values
     public static String rgb(int r, int g, int b) {
 
-        return primaryToHex(r) + primaryToHex(g) + primaryToHex(b);
+        return String.format("%02x%02x%02x", r, g, b).toUpperCase();
     }
 
-    public static String primaryToHex(int p) {
-
-        if (p > 255) {
-            return  "FF";
-        } else if (p <= 0) {
-            return  "00";
-        } else if (p < 16){
-            return "0" + Integer.toHexString(p).toUpperCase();
-        } else {
-            return Integer.toHexString(p).toUpperCase();
-        }
-    }
 }
